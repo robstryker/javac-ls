c6ac75a177bd19d5437c3263e844355db6ae4ffc
Rob Stryker <rob@oxbeef.net>
2026-06-08 16:46:01 -0400

    Fix recalcitrant test
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- framework/tests/org.jboss.tools.javac.ls.jdt.core.dom.test/src/main/java/org/eclipse/jdt/internal/core/dom/javac/JavacDOMParserTest.java
+++ framework/tests/org.jboss.tools.javac.ls.jdt.core.dom.test/src/main/java/org/eclipse/jdt/internal/core/dom/javac/JavacDOMParserTest.java
@@ -29,7 +29,9 @@ import shaded.org.eclipse.jdt.core.dom.ITypeBinding;
 import shaded.org.eclipse.jdt.core.dom.IVariableBinding;
 import shaded.org.eclipse.jdt.core.dom.MethodDeclaration;
 import shaded.org.eclipse.jdt.core.dom.TypeDeclaration;
+import shaded.org.eclipse.jdt.core.dom.SingleVariableDeclaration;
 import shaded.org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+import shaded.org.eclipse.jdt.core.dom.VariableDeclarationStatement;
 
 /**
  * Tests for JavacDOMParser.
@@ -332,5 +334,258 @@ public class JavacDOMParserTest {
 		ITypeBinding returnType = getNameBinding.getReturnType();
 		assertNotNull("Return type binding should not be null", returnType);
 		assertEquals("Return type should be String", "String", returnType.getName());
+	}
+
+	@Test
+	public void testMethodParameterBindings() {
+		String source = """
+			package com.example;
+
+			public class ParamTest {
+				public void process(String name, int count, Object data) {
+					System.out.println(name + count);
+				}
+			}
+			""";
+
+		JavacDOMParser parser = new JavacDOMParser();
+		CompilationUnit cu = parser.parse(source, "ParamTest.java", null, AST.JLS21, null, true);
+
+		assertNotNull("CompilationUnit should not be null", cu);
+		TypeDeclaration type = (TypeDeclaration) cu.types().get(0);
+		MethodDeclaration method = type.getMethods()[0];
+
+		// Test method binding
+		IMethodBinding methodBinding = method.resolveBinding();
+		assertNotNull("Method binding should not be null", methodBinding);
+		assertEquals("Method should have 3 parameters", 3, methodBinding.getParameterTypes().length);
+
+		// Test first parameter (String name)
+		SingleVariableDeclaration param0Decl = (SingleVariableDeclaration) method.parameters().get(0);
+		IVariableBinding param0 = param0Decl.resolveBinding();
+		assertNotNull("Parameter 0 binding should not be null", param0);
+		assertEquals("Parameter 0 name should be 'name'", "name", param0.getName());
+		assertTrue("Parameter 0 should be a parameter", param0.isParameter());
+		assertEquals("Parameter 0 type should be String", "String", param0.getType().getName());
+
+		// Test second parameter (int count)
+		SingleVariableDeclaration param1Decl = (SingleVariableDeclaration) method.parameters().get(1);
+		IVariableBinding param1 = param1Decl.resolveBinding();
+		assertNotNull("Parameter 1 binding should not be null", param1);
+		assertEquals("Parameter 1 name should be 'count'", "count", param1.getName());
+		assertTrue("Parameter 1 should be a parameter", param1.isParameter());
+		assertEquals("Parameter 1 type should be int", "int", param1.getType().getName());
+
+		// Test third parameter (Object data)
+		SingleVariableDeclaration param2Decl = (SingleVariableDeclaration) method.parameters().get(2);
+		IVariableBinding param2 = param2Decl.resolveBinding();
+		assertNotNull("Parameter 2 binding should not be null", param2);
+		assertEquals("Parameter 2 name should be 'data'", "data", param2.getName());
+		assertTrue("Parameter 2 should be a parameter", param2.isParameter());
+		assertEquals("Parameter 2 type should be Object", "Object", param2.getType().getName());
+	}
+
+	@Test
+	public void testLocalVariableBindings() {
+		String source = """
+			package com.example;
+
+			public class LocalVarTest {
+				public void method() {
+					String localVar = "test";
+					int count = 5;
+					Object obj = null;
+				}
+			}
+			""";
+
+		JavacDOMParser parser = new JavacDOMParser();
+		CompilationUnit cu = parser.parse(source, "LocalVarTest.java", null, AST.JLS21, null, true);
+
+		assertNotNull("CompilationUnit should not be null", cu);
+		TypeDeclaration type = (TypeDeclaration) cu.types().get(0);
+		MethodDeclaration method = type.getMethods()[0];
+
+		// Get variable declaration statements from method body
+		List<?> statements = method.getBody().statements();
+		assertEquals("Should have 3 variable declarations", 3, statements.size());
+
+		// Test first local variable (String localVar)
+		VariableDeclarationStatement stmt0 =
+			(VariableDeclarationStatement) statements.get(0);
+		VariableDeclarationFragment frag0 =
+			(VariableDeclarationFragment) stmt0.fragments().get(0);
+		IVariableBinding var0 = frag0.resolveBinding();
+		assertNotNull("Local variable 0 binding should not be null", var0);
+		assertEquals("Variable 0 name should be 'localVar'", "localVar", var0.getName());
+		assertTrue("Variable 0 should not be a field", !var0.isField());
+		assertTrue("Variable 0 should not be a parameter", !var0.isParameter());
+		assertEquals("Variable 0 type should be String", "String", var0.getType().getName());
+
+		// Test second local variable (int count)
+		VariableDeclarationStatement stmt1 =
+			(VariableDeclarationStatement) statements.get(1);
+		VariableDeclarationFragment frag1 =
+			(VariableDeclarationFragment) stmt1.fragments().get(0);
+		IVariableBinding var1 = frag1.resolveBinding();
+		assertNotNull("Local variable 1 binding should not be null", var1);
+		assertEquals("Variable 1 name should be 'count'", "count", var1.getName());
+		assertTrue("Variable 1 should not be a field", !var1.isField());
+		assertTrue("Variable 1 should not be a parameter", !var1.isParameter());
+		assertEquals("Variable 1 type should be int", "int", var1.getType().getName());
+
+		// Test third local variable (Object obj)
+		VariableDeclarationStatement stmt2 =
+			(VariableDeclarationStatement) statements.get(2);
+		VariableDeclarationFragment frag2 =
+			(VariableDeclarationFragment) stmt2.fragments().get(0);
+		IVariableBinding var2 = frag2.resolveBinding();
+		assertNotNull("Local variable 2 binding should not be null", var2);
+		assertEquals("Variable 2 name should be 'obj'", "obj", var2.getName());
+		assertTrue("Variable 2 should not be a field", !var2.isField());
+		assertTrue("Variable 2 should not be a parameter", !var2.isParameter());
+		assertEquals("Variable 2 type should be Object", "Object", var2.getType().getName());
+	}
+
+	@Test
+	public void testSuperTypeBindings() {
+		String source = """
+			package com.example;
+
+			import java.util.ArrayList;
+
+			public class MyList extends ArrayList<String> implements Comparable<MyList> {
+				@Override
+				public int compareTo(MyList other) {
+					return 0;
+				}
+			}
+			""";
+
+		JavacDOMParser parser = new JavacDOMParser();
+		CompilationUnit cu = parser.parse(source, "MyList.java", null, AST.JLS21, null, true);
+
+		assertNotNull("CompilationUnit should not be null", cu);
+		TypeDeclaration type = (TypeDeclaration) cu.types().get(0);
+		ITypeBinding typeBinding = type.resolveBinding();
+
+		assertNotNull("Type binding should not be null", typeBinding);
+		assertEquals("Type name should be MyList", "MyList", typeBinding.getName());
+
+		// Test superclass binding
+		ITypeBinding superclass = typeBinding.getSuperclass();
+		assertNotNull("Superclass binding should not be null", superclass);
+		assertTrue("Superclass should be parameterized", superclass.isParameterizedType());
+		assertEquals("Superclass should be ArrayList", "ArrayList", superclass.getErasure().getName());
+
+		// Test superclass type arguments
+		ITypeBinding[] typeArgs = superclass.getTypeArguments();
+		assertEquals("Should have 1 type argument", 1, typeArgs.length);
+		assertEquals("Type argument should be String", "String", typeArgs[0].getName());
+
+		// Test interfaces
+		ITypeBinding[] interfaces = typeBinding.getInterfaces();
+		assertEquals("Should implement 1 interface", 1, interfaces.length);
+		assertTrue("Interface should be parameterized", interfaces[0].isParameterizedType());
+		assertEquals("Interface should be Comparable", "Comparable", interfaces[0].getErasure().getName());
+	}
+
+	@Test
+	public void testAnnotationBindings() {
+		String source = """
+			package com.example;
+
+			@Deprecated
+			@SuppressWarnings("unused")
+			public class AnnotatedTest {
+				@Override
+				public String toString() {
+					return "test";
+				}
+
+				@Deprecated
+				public void oldMethod() {
+				}
+			}
+			""";
+
+		JavacDOMParser parser = new JavacDOMParser();
+		CompilationUnit cu = parser.parse(source, "AnnotatedTest.java", null, AST.JLS21, null, true);
+
+		assertNotNull("CompilationUnit should not be null", cu);
+		TypeDeclaration type = (TypeDeclaration) cu.types().get(0);
+		ITypeBinding typeBinding = type.resolveBinding();
+
+		assertNotNull("Type binding should not be null", typeBinding);
+
+		// Test type annotations
+		shaded.org.eclipse.jdt.core.dom.IAnnotationBinding[] typeAnnotations = typeBinding.getAnnotations();
+		assertNotNull("Type annotations should not be null", typeAnnotations);
+		assertTrue("Should have at least 1 annotation", typeAnnotations.length >= 1);
+
+		boolean foundDeprecated = false;
+		for (shaded.org.eclipse.jdt.core.dom.IAnnotationBinding annotation : typeAnnotations) {
+			if ("Deprecated".equals(annotation.getAnnotationType().getName())) {
+				foundDeprecated = true;
+				break;
+			}
+		}
+		assertTrue("Should have @Deprecated annotation on type", foundDeprecated);
+
+		// Test method annotations
+		MethodDeclaration[] methods = type.getMethods();
+		assertTrue("Should have at least 2 methods", methods.length >= 2);
+
+		// Find toString method and check @Override
+		MethodDeclaration toStringMethod = null;
+		for (MethodDeclaration method : methods) {
+			if ("toString".equals(method.getName().getIdentifier())) {
+				toStringMethod = method;
+				break;
+			}
+		}
+		assertNotNull("toString method should be found", toStringMethod);
+
+		IMethodBinding toStringBinding = toStringMethod.resolveBinding();
+		assertNotNull("toString binding should not be null", toStringBinding);
+
+		shaded.org.eclipse.jdt.core.dom.IAnnotationBinding[] methodAnnotations = toStringBinding.getAnnotations();
+		assertNotNull("Method annotations should not be null", methodAnnotations);
+		assertTrue("Should have at least 1 annotation", methodAnnotations.length >= 1);
+
+		boolean foundOverride = false;
+		for (shaded.org.eclipse.jdt.core.dom.IAnnotationBinding annotation : methodAnnotations) {
+			if ("Override".equals(annotation.getAnnotationType().getName())) {
+				foundOverride = true;
+				break;
+			}
+		}
+		assertTrue("Should have @Override annotation on toString method", foundOverride);
+
+		// Find oldMethod and check @Deprecated
+		MethodDeclaration oldMethod = null;
+		for (MethodDeclaration method : methods) {
+			if ("oldMethod".equals(method.getName().getIdentifier())) {
+				oldMethod = method;
+				break;
+			}
+		}
+		assertNotNull("oldMethod should be found", oldMethod);
+
+		IMethodBinding oldMethodBinding = oldMethod.resolveBinding();
+		assertNotNull("oldMethod binding should not be null", oldMethodBinding);
+
+		shaded.org.eclipse.jdt.core.dom.IAnnotationBinding[] oldMethodAnnotations = oldMethodBinding.getAnnotations();
+		assertNotNull("oldMethod annotations should not be null", oldMethodAnnotations);
+		assertTrue("Should have at least 1 annotation", oldMethodAnnotations.length >= 1);
+
+		boolean foundDeprecatedOnMethod = false;
+		for (shaded.org.eclipse.jdt.core.dom.IAnnotationBinding annotation : oldMethodAnnotations) {
+			if ("Deprecated".equals(annotation.getAnnotationType().getName())) {
+				foundDeprecatedOnMethod = true;
+				break;
+			}
+		}
+		assertTrue("Should have @Deprecated annotation on oldMethod", foundDeprecatedOnMethod);
 	}
 }
